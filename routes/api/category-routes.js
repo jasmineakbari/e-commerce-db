2eba58bc9653a46506644fa7d2aa07ba47feebcb
unknown <jasmineakbari@yahoo.com>
2021-01-31 17:27:56 -0500

    created find categories route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,25 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock']
+      }
+    ]
+  })
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(400).json({ message: 'Unable to locate Categories' });
+        return;
+      }
+      res.json(dbCategoryData)''
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 router.get('/:id', (req, res) => {
